1dc6fa67f8809fd49931cf30575b5f69c440f48d
JMHeartley <67245523+JMHeartley@users.noreply.github.com>
2021-02-14 14:11:24 -0500

    Added Member model

---
+++ PicsOfUs/Models/Member.cs
@@ -0,0 +1,27 @@
+﻿namespace PicsOfUs.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string FirstNameA { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public Suffix? Suffix { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+        Other = 3
+    }
+
+    public enum Suffix
+    {
+        I = 1,
+        II = 2,
+        III = 3,
+        IV = 4,
+        V = 5
+    }
+}
