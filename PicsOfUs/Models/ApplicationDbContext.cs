acd5de11059889de2a2b6b96ce0bcf040e032216
JMHeartley <67245523+JMHeartley@users.noreply.github.com>
2021-02-14 14:14:08 -0500

    Moved DbContext to its own file

---
+++ PicsOfUs/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PicsOfUs.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
