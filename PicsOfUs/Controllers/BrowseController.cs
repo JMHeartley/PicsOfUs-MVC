3a00978a612cfb53cc9f971bb27c94b5113d842e
JMHeartley <67245523+JMHeartley@users.noreply.github.com>
2021-02-17 17:33:10 -0500

    Added Browse Controller and basic Index view

---
+++ PicsOfUs/Controllers/BrowseController.cs
@@ -0,0 +1,28 @@
+﻿using PicsOfUs.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace PicsOfUs.Controllers
+{
+    public class BrowseController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public BrowseController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ActionResult Index()
+        {
+            var photos = _context.Photos.ToList();
+
+            return View(photos);
+        }
+    }
+}
