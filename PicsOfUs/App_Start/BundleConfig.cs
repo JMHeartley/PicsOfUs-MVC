c5fc7f06d7e000715ce6e66ce37949ac7a19dc09
JMHeartley <67245523+JMHeartley@users.noreply.github.com>
2021-02-18 18:04:39 -0500

    Updating default scaffolding to fit Bootstrap 4

--- PicsOfUs/App_Start/BundleConfig.cs
+++ PicsOfUs/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace PicsOfUs
 {
@@ -23,7 +22,7 @@ namespace PicsOfUs
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-4-lumen.css",
                       "~/Content/site.css"));
         }
     }
