657a419317369d3eb6a34b0fd2ffc76f2d1a84bc
JMHeartley <67245523+JMHeartley@users.noreply.github.com>
2021-02-17 20:43:57 -0500

    Added AgeInPhoto method and Birthday property to Member

--- PicsOfUs/Models/Member.cs
+++ PicsOfUs/Models/Member.cs
@@ -23,6 +23,27 @@ namespace PicsOfUs.Models
                 return FirstName + " " + LastName + " " + Suffix;
             }
         }
+
+        public string Birthday
+        {
+            get
+            {
+                if (BirthDate == null)
+                    return "";
+
+                var birthdate = BirthDate.GetValueOrDefault();
+
+                return $"{(Month)birthdate.Month} {birthdate.Day}";
+            }
+        }
+
+        public int? AgeInPhoto(DateTime? captureDateTime)
+        {
+            if (BirthDate == null || captureDateTime == null)
+                return null;
+
+            return (int)((captureDateTime.GetValueOrDefault() - BirthDate.GetValueOrDefault()).TotalDays / 365.25);
+        }
     }
 
     public enum Gender
@@ -39,5 +60,22 @@ namespace PicsOfUs.Models
         III = 3,
         IV = 4,
         V = 5
+    }
+
+    public enum Month
+    {
+        NotSet = 0,
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12
     }
 }
