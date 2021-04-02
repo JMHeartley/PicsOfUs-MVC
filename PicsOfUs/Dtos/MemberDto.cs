5d78729c1279ef0788bc00cd9fc3c51f8d03cba1
Justin <heartleyjm@gmail.com>
2021-04-01 22:04:50 -0400

    Installed Automapper and created Members Api Controller

---
+++ PicsOfUs/Dtos/MemberDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PicsOfUs.Models;
+
+namespace PicsOfUs.Dtos
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public Suffix? Suffix { get; set; }
+        public ICollection<Member> Siblings { get; set; }
+        public ICollection<Member> Parents { get; set; }
+        public ICollection<Member> Children { get; set; }
+    }
+}
