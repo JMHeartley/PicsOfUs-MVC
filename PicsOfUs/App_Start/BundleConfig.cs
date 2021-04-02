44e861d5d43e844b3f112529b20b3596b9538f82
JHeartley <heartleyjm@gmail.com>
2021-04-02 16:40:03 -0400

    Added jQuery Datatables to the project

--- PicsOfUs/App_Start/BundleConfig.cs
+++ PicsOfUs/App_Start/BundleConfig.cs
@@ -7,8 +7,13 @@ namespace PicsOfUs
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery-{version}.js", 
+                        "~/Scripts/bootbox.js",
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/datatables/datatables.bootstrap.js",
+                        "~/Scripts/datatables/jquery.datatables.js",
+                        "~/Scripts/popper.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,16 +23,10 @@ namespace PicsOfUs
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootbox.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-4-lumen.css",
+                      "~/Content/datatables/css/datatables.bootstrap.css",
                       "~/Content/site.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/popper").Include(
-          "~/Scripts/popper.js"));
         }
     }
 }
