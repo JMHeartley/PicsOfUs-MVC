7b8c2e232d721a3d80f0b8127fb3166cefd56a9d
Justin <heartleyjm@gmail.com>
2021-04-24 15:44:34 -0400

    Adding member details page to lightbox (Big Changes)

--- PicsOfUs/Scripts/My Scripts/lightbox.js
+++ PicsOfUs/Scripts/My Scripts/lightbox.js
@@ -1,35 +1,35 @@
 $(function () {
-  let navbarDesktop = $('#navbar-desktop');
-  let navbarMobile = $('#navbar-mobile');
+  const navbarDesktop = $('#navbar-desktop');
+  const navbarMobile = $('#navbar-mobile');
 
-  let lightboxContainer = $('#lightbox-container');
-  let lightboxCloserButton = $('#lightbox-closer-button');
-  let memberDetailsSection = $('#member-details');
-  let lovePic = $('#love-pic');
-  let leftArrow = $('#left-arrow');
-  let rightArrow = $('#right-arrow');
-  let mobileNavHeight =
-    navbarMobile.height() +
+  const lightboxContainer = $('#lightbox-container');
+  const lightboxCloserButton = $('#lightbox-closer-button');
+  const memberDetailsSection = $('#member-details');
+  const lovePic = $('#love-pic');
+  const leftArrow = $('#left-arrow');
+  const rightArrow = $('#right-arrow');
+  const mobileNavHeight = navbarMobile.height() +
     parseInt(navbarMobile.css('padding-top')) +
     parseInt(navbarMobile.css('padding-bottom'));
 
   //#region Move to site.js
 
   // dynamically set the height of the mobile nav buttons, which will overlay on the mobile navbar
-  let mobileNavButtons = $('.mobile-nav-button');
+  const mobileNavButtons = $('.mobile-nav-button');
+  const mobileNavBuffers = $('.add-mobile-nav-buffer');
   // let mobileNavHeight =
   //   navbarMobile.height() +
   //   parseInt(navbarMobile.css('padding-top')) +
   //   parseInt(navbarMobile.css('padding-bottom'));
-  console.log('mobileNavHeight', mobileNavHeight);
-  mobileNavButtons.height(mobileNavHeight);
+    mobileNavButtons.height(mobileNavHeight);
+    mobileNavBuffers.css('padding-bottom', mobileNavHeight);
 
-  //#endregion
+    //#endregion
 
   //#region Event Listener Setup
 
   $('#results-body').on('click', '.trigger', function () {
-    let resultId = parseInt($(this).attr('data-result-id'));
+    const resultId = parseInt($(this).attr('data-result-id'));
     openLightbox(resultId);
   });
   $('.lightbox-closer').on('click', '', function () {
@@ -47,7 +47,11 @@ $(function () {
   $('#pic-subjects').on('click', '.fake-profile', function () {
     openMemberDetails(parseInt($(this).attr('data-member-id')));
   });
-  $('#back').on('click', '', function () {
+  memberDetailsSection.on('click', '.fake-profile', function () {
+    resetMemberDetails();
+    openMemberDetails(parseInt($(this).attr('data-member-id')));
+  });
+  $('.member-details-closer').on('click', '', function () {
     closeMemberDetails();
   });
 
@@ -61,14 +65,14 @@ $(function () {
 
   //#endregion
 
-  //#region Lightbox Functions
+  //#region Functions
 
   function openLightbox(resultId) {
     lightboxContainer.removeClass('hidden');
     navbarDesktop.addClass('hidden');
     navbarMobile.addClass('hidden');
     moveToPic(resultId);
-    lightboxContainer.css('padding-bottom', mobileNavHeight);
+    //lightboxContainer.css('padding-bottom', mobileNavHeight);
     $('body').addClass('restrict-scroll');
   }
 
@@ -78,13 +82,15 @@ $(function () {
     navbarDesktop.removeClass('hidden');
     navbarMobile.removeClass('hidden');
     closeMemberDetails();
-    lightboxContainer.css('padding-bottom', 0);
+    //lightboxContainer.css('padding-bottom', 0);
     $('body').removeClass('restrict-scroll');
   }
 
   function moveToPic(resultId) {
-    
-    let resultPicMaxId = parseInt(
+
+    lightboxContainer.scrollTop(0);
+
+    const resultPicMaxId = parseInt(
       $('#results-body .photo-result:last-child').attr('data-result-id')
     );
     leftArrow.removeClass('hidden');
@@ -93,86 +99,56 @@ $(function () {
     if (resultId === resultPicMaxId) rightArrow.addClass('hidden');
     else if (resultId === 0) leftArrow.addClass('hidden');
 
-    let photoId = $('#results-body')
-      .find('[data-result-id=' + resultId + ']')
+    const photoId = $('#results-body')
+      .find(`[data-result-id=${resultId}]`)
       .attr('data-photo-id');
-    let cachedPhotoMembers;
-    let cachedCaptureDate;
-    let def = $.Deferred();
+    const def = $.Deferred();
     def
       .then(function () {
-        return $.get('/api/photos/' + photoId).done(function (photo) {
+        return $.get(`/api/photos/${photoId}`).done(function (photo) {
           console.log('photo loaded to lightbox', photo);
-          
+
           lightboxContainer.attr('data-result-id', resultId);
 
-          let pic = $('#lightbox-pic');
+          const pic = $('#lightbox-pic');
           pic.attr('src', photo.url);
           pic.attr('alt', photo.caption);
 
           $('#caption').text(photo.caption);
 
-          let captureArea = $('#capture-date');
-          let formattedDate;
+          const captureArea = $('#capture-date');
 
           if (photo.captureDate !== null) {
-            formattedDate = new Date(photo.captureDate);
+            const formattedDate = new Date(photo.captureDate);
             captureArea.text(formattedDate.toLocaleDateString());
             captureArea.removeClass('hidden');
           } else {
             captureArea.addClass('hidden');
-            console.log(formattedDate);
           }
-
-          console.log('photo.captureDate', formattedDate);
-
-          cachedPhotoMembers = photo.members;
-          cachedCaptureDate = formattedDate;
 
           // set initial loved pic val
           // toggleLovePic(!photo.isLoved);
         });
       })
-      .then(function () {
-        return $.get('/Static/PicProfile.html').done(function (picProfile) {
-          console.log('loading photo.members', cachedPhotoMembers);
-          let subjectsArea = $('#pic-subjects');
+      .then(function (photo) {
+        return $.get('/Static/PicProfile.html').done(function (emptyProfile) {
+
+          console.log('html loaded', emptyProfile);
+
+          const subjectsArea = $('#pic-subjects');
           subjectsArea.html('');
 
-          if (cachedPhotoMembers !== undefined) {
-            $.each(cachedPhotoMembers, function (index, element) {
-              let newProfile = $(picProfile).clone();
+          if (photo.members) {
+            $.each(photo.members, function (index, member) {
 
-              newProfile
-                .filter('.fake-profile')
-                .attr('data-member-id', element.id);
-
-              newProfile.find('.name').text(element.name);
-
-              let ageArea = newProfile.find('.age');
-
-              if (cachedCaptureDate === undefined) {
-                ageArea.addClass('hidden');
-              } else {
-                let birthDate = new Date(element.birthDate);
-                let yearsInMilliseconds = 1000 * 3600 * 24 * 365;
-                let ageInYears = Math.floor(
-                  (cachedCaptureDate.getTime() - birthDate.getTime()) /
-                  yearsInMilliseconds
-                );
-                ageArea.append(ageInYears);
-              }
-
-              newProfile.appendTo(subjectsArea);
+              const picProfile = insertIntoProfile(member, emptyProfile, photo.captureDate);
+              picProfile.appendTo(subjectsArea);
             });
           }
         });
       })
       .fail(function (error) {
         console.log('there was an error!, log to global logger', error);
-      })
-      .always(function () {
-        lightboxContainer.scrollTop(0);
       });
     def.resolve();
   }
@@ -184,11 +160,11 @@ $(function () {
 
     $('#love-pic').attr('data-is-loved', isLoved.toString());
 
-    let lovedPicIcon = lovePic.children('.fa-heart');
-    let lovedPicText = lovePic.children('span');
+    const lovedPicIcon = lovePic.children('.fa-heart');
+    const lovedPicText = lovePic.children('span');
 
     if (isLoved) {
-      lovedPicText.text('Loved Pic');
+      lovedPicText.text('Pic Loved');
       lovedPicIcon.addClass('fas');
       lovedPicIcon.removeClass('far');
     } else {
@@ -201,36 +177,106 @@ $(function () {
 
   function openMemberDetails(memberId) {
 
-    console.log('open the details section for member with id: ' + memberId);
-    memberDetailsSection.removeClass('hidden');
+    console.log(`open the details section for member with id: ${memberId}`);
 
-    $.ajax({
-      url: '/api/members/' + memberId,
-      type: 'GET',
-      contentType: 'application/json',
-    })
-      .done(function (data) {
-        console.log("loading member-details", data);
+    const def = $.Deferred();
+    def
+      .then(function () {
+        return $.get(`/api/members/${memberId}`).done(function (data) {
+          console.log('loading member-details', data);
 
-        memberDetailsSection.find('#member-name').text(data.name);
+          memberDetailsSection.find('#member-name').text(data.name);
 
-        let birthDate = new Date(data.birthDate);
-        let birthday = birthDate.getMonth() + 1 + ' ' + birthDate.getDate();
-        memberDetailsSection.find('#member-birthday').text(birthday);
+            const months =
+                ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+                    'August', 'September', 'October', 'November', 'December'];
+            const birthDate = new Date(data.birthDate);
+            const birthMonth = months[birthDate.getMonth()];
+          const birthday = birthMonth + ' ' + birthDate.getDate();
+          memberDetailsSection.find('#member-birthday').text(birthday);
+        });
+      })
+      .then(function (member) {
+        return $.get('/Static/PicProfile.html').done(function (emptyProfile) {
+
+          const parentsArea = $('#member-parents');
+
+
+          $.each(member.parents, function (index, parent) {
+            console.log('insert parent', parent.name);
+
+            const newProfile = insertIntoProfile(parent, emptyProfile);
+            newProfile.appendTo(parentsArea);
+          });
+
+          const siblingsArea = $('#member-siblings');
+
+          $.each(member.siblings, function (index, sibling) {
+            console.log('insert sibling', sibling.name);
+
+            const newProfile = insertIntoProfile(sibling, emptyProfile);
+            newProfile.appendTo(siblingsArea);
+          });
+
+          const childrenArea = $('#member-children');
+
+          $.each(member.children, function (index, child) {
+            console.log('insert child', child.name);
+
+            const newProfile = insertIntoProfile(child, emptyProfile);
+            newProfile.appendTo(childrenArea);
+          });
+        });
       })
       .fail(function (error) {
         console.log('member call failed, display message and log to global logger', error);
       })
       .always(function () {
         lightboxContainer.scrollTop(0);
-        lightboxContainer.addClass("restrict-scroll");
+        lightboxContainer.addClass('restrict-scroll');
+          memberDetailsSection.removeClass('hidden');
+          //memberDetailsSection.css('padding-bottom', mobileNavHeight);
       });
+    def.resolve();
   }
 
   function closeMemberDetails() {
     memberDetailsSection.addClass('hidden');
-    lightboxContainer.removeClass("restrict-scroll");
+      lightboxContainer.removeClass('restrict-scroll');
+      //memberDetailsSection.css('padding-bottom', 0);
   }
 
+  function resetMemberDetails() {
+    memberDetailsSection.find('#member-parents').empty();
+    memberDetailsSection.find('#member-siblings').empty();
+    memberDetailsSection.find('#member-children').empty();
+  }
+
+  function insertIntoProfile(member, emptyProfile, captureDate) {
+    console.log(`insertIntoProfile: member: ${member}\ncaptureDate: ${captureDate}\npicProfile: ${emptyProfile}`);
+    const cloneProfile = $(emptyProfile).clone();
+
+    cloneProfile
+      .filter('.fake-profile')
+      .attr('data-member-id', member.id);
+
+    cloneProfile.find('.name').text(member.name);
+
+    const ageArea = cloneProfile.find('.age');
+    if (captureDate) {
+
+      const captureDateClass = Date.parse(captureDate);
+      const birthDateClass = Date.parse(member.birthDate);
+      const timeDifferenceInMilliseconds = Math.abs(captureDateClass - birthDateClass);
+
+      const yearsInMilliseconds = 1000 * 3600 * 24 * 365;
+      const ageInYears = Math.floor(timeDifferenceInMilliseconds / yearsInMilliseconds);
+      ageArea.append(ageInYears);
+
+      ageArea.removeClass('hidden');
+    }
+
+    return cloneProfile;
+  }
   //#endregion
 });
