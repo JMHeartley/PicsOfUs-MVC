dbb9b394bcdaf754aa8c10112060b03c262c3109
Justin <heartleyjm@gmail.com>
2021-08-21 15:48:30 -0400

    Added NLog to the project
    Installed NLog, added a Logs folder with a .gitignore. I also had to reinstall Entity Framework 6

---
+++ PicsOfUs/Utilities/ILogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Web;
+
+namespace PicsOfUs.Utilities
+{
+    public interface ILogger
+    {
+        void Debug(string message, string arguments = null);
+        void Info(string message, string args = null);
+        void Warning(string message, string args = null);
+        void Error(string message, string args = null);
+    }
+}
---
+++ PicsOfUs/Utilities/NLogger.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Web;
+using NLog;
+
+namespace PicsOfUs.Utilities
+{
+    public class NLogger : ILogger
+    {
+        private static NLogger _instance;
+        private static Logger _logger;
+        private const string RuleName = "LoggerRule";
+
+        public static NLogger GetInstance()
+        {
+            if (_instance == null)
+                _instance = new NLogger();
+            
+            return _instance;
+        }
+
+        public static Logger GetLogger(string theLogger)
+        {
+            if (_logger == null)
+                _logger = LogManager.GetLogger(theLogger);
+
+            return _logger;
+        }
+
+        public void Debug(string message, string arguments = null)
+        {
+            if (arguments == null)
+                GetLogger(RuleName).Debug(message);
+            else
+                GetLogger(RuleName).Debug(message, arguments);
+        }
+
+        public void Info(string message, string arguments = null)
+        {
+            if (arguments == null)
+                GetLogger(RuleName).Info(message);
+            else
+                GetLogger(RuleName).Info(message, arguments);
+        }
+
+        public void Warning(string message, string arguments = null)
+        {
+            if (arguments == null)
+                GetLogger(RuleName).Warn(message);
+            else
+                GetLogger(RuleName).Warn(message, arguments);
+        }
+
+        public void Error(string message, string arguments = null)
+        {
+            if (arguments == null)
+                GetLogger(RuleName).Error(message);
+            else
+                GetLogger(RuleName).Error(message, arguments);
+        }
+    }
+}
